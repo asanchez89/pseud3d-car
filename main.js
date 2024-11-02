86de7f3ff970b80b992f03b093d3c99cd9ed8d62
asanchez89 <asanchez@deltare.net>
2024-11-01 21:08:30 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const SCREEN_W = 1920;
+const SCREEN_H = 1080;
+
+const SCREEN_CX = SCREEN_W/2;
+const SCREEN_CY = SCREEN_H/2;
+
+class MainScene extends Phaser.Scene{
+    constructor(){
+        super({key: 'SceneMain'});
+    }
+}
+
+class
